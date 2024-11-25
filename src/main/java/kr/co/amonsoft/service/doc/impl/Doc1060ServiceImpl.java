dfbf83d536cb3c2721fc279c3017192c97acaad5
LOQ <LOQ@DESKTOP-03S3QL1>
2024-11-25 16:39:58 +0900

    결재 수정

--- src/main/java/kr/co/amonsoft/service/doc/impl/Doc1060ServiceImpl.java
+++ src/main/java/kr/co/amonsoft/service/doc/impl/Doc1060ServiceImpl.java
@@ -24,20 +24,29 @@ public class Doc1060ServiceImpl implements Doc1060Service {
     private final Doc1060Mapper doc1060Mapper;
     
 	@Override
-	public BigInteger insertTransportExpense(Map<String, Object> param) {
-        docCommonMapper.insertDocument(param);
-        BigInteger docId = (BigInteger) param.get("docId");
+	public BigInteger insertTransportExpense(Map<String, Object> approvalData) {
+		List<Map<String, Object>> approvalStep = (List<Map<String, Object>>) approvalData.get("approvalData");
+		
+		String currentApproverId = approvalStep.stream()
+	            .filter(step -> Integer.valueOf(String.valueOf(step.get("approvalStepNo"))) == 2) // approvalStepNo는 Integer로 처리
+	            .map(step -> String.valueOf(step.get("userId"))) // userId는 String으로 변환
+	            .findFirst()
+	            .orElseThrow(() -> new RuntimeException("Approval step with stepNo 2 is missing"));
 
-        List<Map<String, Object>> approvalStep = (List<Map<String, Object>>) param.get("approvalData");
+	    approvalData.put("currentApproverId", currentApproverId);
+		
+		docCommonMapper.insertDocument(approvalData);
+		BigInteger docId = (BigInteger) approvalData.get("docId");
+
         approvalStep.forEach(step -> {
             step.put("docId", docId);
             apvCommonMapper.insertApprovalStep(step);
         });
 
-        List<Map<String, Object>>expenseDetail = (List<Map<String, Object>>) param.get("data");
+        List<Map<String, Object>>expenseDetail = (List<Map<String, Object>>) approvalData.get("data");
         expenseDetail.forEach(detail -> {
             detail.put("docId", docId);
-            detail.put("userId", param.get("userId"));
+            detail.put("userId", approvalData.get("userId"));
             doc1060Mapper.insertTransportExpense(detail);
         });
 
